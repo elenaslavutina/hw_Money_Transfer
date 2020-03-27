25ac29a4d8f517bb9c568144ebd320bb30d97d03
elenaslavutina <elena.slavutina2013@gmail.com>
2020-03-27 15:11:11 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int balansenow = 2_000_000_000;
+	int transaction = 5_000_000;
+	long result = balansenow*transaction;
+	System.out.println(result);
+    }
+}
