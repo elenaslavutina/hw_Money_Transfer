c57185f6d41881abf3650537fff4e7e97eeada83
elenaslavutina <elena.slavutina2013@gmail.com>
2020-03-28 16:52:35 +0300

    main is changed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
 
-    public static void main(String[] args) {
-	int balansenow = 2_000_000_000;
-	int transaction = 5_000_000;
-	int result = balansenow + transaction;
-	System.out.println (result);
+    public static void main(String[] args)
+	{
+        int balansenow = 2_000_000_000;
+        int transaction = 5_000_000;
+        // predel - предельное число транзакций, больше которых произойдет переполение разряда
+        int predel = (2_147_483_647 - balansenow) / transaction;
+        int result1 = balansenow + (predel ) * transaction;
+		System.out.println("Результат для предельного количества транзакций" + " =  " + predel);
+		System.out.println(result1);
+	   // при числе транзакций, на единицу больше предельной ошибка
+        int result2 = balansenow + (predel + 1) * transaction;
+        System.out.println("Результат, если количество транзакций больше предельной");
+		System.out.println(result2);
     }
 }
