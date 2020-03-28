4a439bf4ae5a9b90ef56659bae49c763fa4333bf
elenaslavutina <elena.slavutina2013@gmail.com>
2020-03-28 16:54:53 +0300

    picture is added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	{
         int balansenow = 2_000_000_000;
         int transaction = 5_000_000;
-        // predel - предельное число транзакций, больше которых произойдет переполение разряда
+        // predel - предельное число транзакций, больше которых произойдет переполение разряда int
         int predel = (2_147_483_647 - balansenow) / transaction;
         int result1 = balansenow + (predel ) * transaction;
 		System.out.println("Результат для предельного количества транзакций" + " =  " + predel);
