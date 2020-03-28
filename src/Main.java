6afe8fe16e6b7d19386df2cb4509f4d6284a4547
elenaslavutina <elena.slavutina2013@gmail.com>
2020-03-28 19:57:18 +0300

    code clean up

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         // predel - предельное число транзакций, больше которых произойдет переполение разряда int
         int predel = (2_147_483_647 - balansenow) / transaction;
         int result1 = balansenow + (predel ) * transaction;
-		System.out.println("Результат для предельного количества транзакций" + " =  " + predel);
+		System.out.println("Результат для предельного количества транзакций =  " + predel);
 		System.out.println(result1);
-	   // при числе транзакций, на единицу больше предельной ошибка
+	    // при числе транзакций, на единицу больше предельной ошибка
         int result2 = balansenow + (predel + 1) * transaction;
         System.out.println("Результат, если количество транзакций больше предельной");
 		System.out.println(result2);
